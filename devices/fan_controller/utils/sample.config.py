abf71e5a149fac30eafb93aa7a7de9d42808634d
Michele Di Girolamo <m.digirolamo@reply.it>
2019-06-15 11:32:45 +0200

    Updated configuration files

--- devices/fan_controller/utils/sample.config.py
+++ devices/fan_controller/utils/sample.config.py
@@ -5,7 +5,7 @@ KEYFILE = "./certificates/private.pem.key"
 MQTT_ENDPOINT = "<AWS IoT MQTT Broker endpoint>"
 MQTT_PORT = "8883"
 MQTT_IDENTITY = "fan-controller"
-SUB_TOPIC = 'xchange_2019/iot_alarm_app/notifications'
+SUB_TOPIC = 'xchange/notifications'
 
 # RELAY
 RELAY_PIN = 10
--- devices/temperature_sensor/utils/sample.config.py
+++ devices/temperature_sensor/utils/sample.config.py
@@ -5,8 +5,8 @@ KEYFILE = "./certificates/private.pem.key"
 MQTT_ENDPOINT = "<AWS IoT MQTT Broker endpoint>"
 MQTT_PORT = "8883"
 MQTT_IDENTITY = "temperature-sensor"
-PUB_TOPIC = 'xchange_2019/iot_alarm_app/measurements'
+PUB_TOPIC = 'xchange/measurements'
 
 # ThingsBoard
-THINGSBOARD_HOST="demo.thingsboard.io"
-ACCESS_TOKEN="<Access token>"
+THINGSBOARD_HOST = "demo.thingsboard.io"
+ACCESS_TOKEN = "<Access token>"
